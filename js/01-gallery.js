02d8700293c6baf48effcfa02af039f28a50ac2e
artyomakulov <artyomakulov@gmail.com>
2021-12-16 00:48:47 +0200

    test

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const galleryItemsMarkup = createGalleryItem(galleryItems);
+gallery.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
+
+
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+            <li class="gallery__item"><a class="gallery__link" href="${preview}"><img class="gallery__image" src="${original}" data-source="${preview}" alt="${description}"/></a></li>`;})
+    .join('');
+}
+
+function onImageClick(event) {
+    event.preventDefault();
+    const instance = basicLightbox.create(`<img scr="${event.target.src}">`,);
+    instance.show();
+    }
+
+gallery.addEventListener('click', onImageClick);
+console.log(basicLightbox);
+
+
+
