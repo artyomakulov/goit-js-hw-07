bba11f9712df648fbdcc2fe00180d41b777a72a3
artyomakulov <artyomakulov@gmail.com>
2021-12-16 01:43:11 +0200

    1е задание

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const gallery = document.querySelector('.gallery');
-
+const lightboxEl = document.querySelector('.modal')
 const galleryItemsMarkup = createGalleryItem(galleryItems);
 gallery.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
 
@@ -11,13 +11,13 @@ function createGalleryItem(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
-            <li class="gallery__item"><a class="gallery__link" href="${preview}"><img class="gallery__image" src="${original}" data-source="${preview}" alt="${description}"/></a></li>`;})
+            <li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></li>`;})
     .join('');
 }
 
 function onImageClick(event) {
     event.preventDefault();
-    const instance = basicLightbox.create(`<img scr="${event.target.src}">`,);
+    const instance = basicLightbox.create(`<div class="modal"><img src="${event.target.dataset.source}"></div>`,);
     instance.show();
     }
 
@@ -26,3 +26,26 @@ console.log(basicLightbox);
 
 
 
+//  document.addEventListener('keydown', closeModal);
+//     function closeModal(event) {
+//         if (event.code === "Escape") {
+//             document.querySelector('.gallery__image').style.displey = "none";
+//         }
+//     }
+
+
+
+// basicLightbox.addEventListener('click', closeModal);
+// function closeModal(event) {
+//   if (event.target.classList.contains('basicLightbox')) {
+//     closeModalClick();
+//   }
+//   if (event.target.classList.contains('basicLightbox')) {
+//     closeModalClick();
+//   }
+//   if (event.code === 'Escape') {
+//     closeModalClick();
+//   }
+// }
+
+// lightboxEl.addEventListener('click', closeModal);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const lightboxEl = document.querySelector('.modal')
+const galleryItemsMarkup = createGalleryItem(galleryItems);
+gallery.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
 
-console.log(galleryItems);
+
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+            <li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></li>`;})
+    .join('');
+}
+
+function onImageClick(event) {
+    event.preventDefault();
+    const instance = basicLightbox.create(`<div class="modal"><img src="${event.target.dataset.source}"></div>`,);
+    instance.show();
+    }
+
+gallery.addEventListener('click', onImageClick);
+console.log(basicLightbox);
