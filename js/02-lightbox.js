c8f924f1eb7872f3dd304310ae73f063d660b60c
artyomakulov <artyomakulov@gmail.com>
2021-12-16 20:36:40 +0200

    2е дз

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,25 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const gallery = document.querySelector('.gallery');
-const lightboxEl = document.querySelector('.modal')
+
 const galleryItemsMarkup = createGalleryItem(galleryItems);
 gallery.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
 
 
 
 function createGalleryItem(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `
-            <li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></li>`;})
-    .join('');
+return galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"></a>`;})
+.join('');
 }
 
-function onImageClick(event) {
-    event.preventDefault();
-    const instance = basicLightbox.create(`<div class="modal"><img src="${event.target.dataset.source}"></div>`,);
-    instance.show();
-    }
-
-gallery.addEventListener('click', onImageClick);
-console.log(basicLightbox);
+let gallerySet = new SimpleLightbox('.gallery a', {captionPosition: "bottom", captionDelay: 250});
+gallerySet.on('show.simplelightbox', function () {
+  return;
+});
